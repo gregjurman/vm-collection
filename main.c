840e7623dc5e6dd24432d405cc0eb3676467e2c9
Greg Jurman <gdj2214@rit.edu>
2012-03-15 20:03:50 -0400

    Added more commenting, moved registers into our acutal VM

--- main.c
+++ main.c
@@ -17,10 +17,8 @@
 #define OP_COPY     0
 
 
-// We have 4 registers
-char registers[4] = {0};
 
-void inspect_registers() {
+void inspect_registers(char registers[]) {
     // Being lazy and not bothering looping
     printf("\tCurrent registers: %i %i %i %i\n", 
         registers[0], registers[1],
@@ -64,14 +62,57 @@ typedef struct {
 
 // Our virtual machine
 int dispatch(FILE * fp) {
-    operation_struct op; // Our current operation
-    size_t op_num = 0; // Keep count how many instructions we have done
+    // We have 4 registers
+    char registers[4] = {0};
+
+    // Our current operation
+    operation_struct op;
+
+    // Lets keep count of how many instructions we have proccessed
+    size_t op_num = 0; 
+
+    /* This defines how GCC contructs our switch statement
+     * 
+     * When you wrap one of our opcodes with TARGET() GCC will fill in the
+     * blanks with the definition below.
+     *
+     * Example:
+     *
+     * `TARGET(OP_DISPLAY)` will become:
+     * 
+     * `TARGET_OP_DISPLAY:
+     *  case OP_DISPLAY:`
+     *
+     * This is why we do not add a case statement or : after TARGET() as it
+     * becomes filled in for us before the compiler does it's thing 
+     * (hence being a pre-processor command).
+     *
+     * Example:
+     *
+     * `TARGET(OP_DISPLAY)
+     *  printf("%i", op.data.input);
+     *  break;`
+     *
+     * becomes:
+     *
+     * `TARGET_OP_DISPLAY:
+     *  case OP_DISPLAY:
+     *  printf("%i", op.data.input);
+     *  break;`
+    **/
 
     #define TARGET(op) \
         TARGET_##op: \
         case op: 
 
-    // Define targets (these let us haul ass)
+    /* After pre-processing adds the appropriate labels to the code
+     * we define our target list in a static array for our opcodes.
+     *
+     * This way we can take the numerical value of our opcode 
+     * (for example, OP_DISPLAY is 3) and reference it to a list rather
+     * than evaluating it against a switch/case.
+    **/
+
     static void * op_targets[4] = {
         &&TARGET_OP_COPY,
         &&TARGET_OP_READ,
@@ -81,11 +122,11 @@ int dispatch(FILE * fp) {
 
     // Our main VM loop
     while(fp != NULL) {
-        dispatch:
+        DISPATCH:
         op_num++;
 
         printf("\n\nDispatching operation number: %li\n", op_num);
-        inspect_registers();
+        inspect_registers(registers);
 
         // Read in a new 2-byte command, if not break
         if(fread(&op, sizeof(operation_struct), 1, fp) != 1) break;
@@ -102,19 +143,19 @@ int dispatch(FILE * fp) {
         switch(op.cmd.opcode) {
             TARGET(OP_DISPLAY) // Pass-through
                 printf("%i ", op.data.input);
-                goto dispatch;
+                goto DISPATCH;
 
             TARGET(OP_WRITE) // Write to register
                 registers[op.cmd.dest] = op.data.input;
-                goto dispatch;
+                goto DISPATCH;
     
             TARGET(OP_READ) // Read(print) from register
                 printf("%i ", registers[op.cmd.src]);
-                goto dispatch;
+                goto DISPATCH;
 
             TARGET(OP_COPY) // Copy from source to destination register
                 registers[op.cmd.dest] = registers[op.cmd.src];
-                goto dispatch;
+                goto DISPATCH;
         }
     }
 
