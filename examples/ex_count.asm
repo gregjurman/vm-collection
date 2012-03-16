7ef2e13eeaa7e5b5a42f7bb43144d758ae8c9c8f
Greg Jurman <gdj2214@rit.edu>
2012-03-16 11:46:02 -0400

    Edited example a bit

--- examples/ex_count.asm
+++ examples/ex_count.asm
@@ -1,6 +1,10 @@
-# I am a comment
-
-    WRT R0, 1
-    WRT R1, 2
-    WRT R2, 3
-    WRT R3, 4
+    LI #1, R0
+    LI #2, R1
+    LI #3, R2
+    LI #4, R3
+    ST R3
+    ST R2
+    ST R1
+    ST R0
+   MOV R0, R3
+    ST R3
