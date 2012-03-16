e5c205aa67a2651998ee312a6fc99d8fb13fdcbf
Greg Jurman <gdj2214@rit.edu>
2012-03-16 11:45:53 -0400

    Finished assembler

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -9,14 +9,15 @@
     2-bits for addressing.
 """
 
-import array
+import struct
 import re
+import argparse
 
 # Define our opcode mapping
 opcodes = { #op, src, dest
     'MOV' : (0, [('reg', 'reg')]), # Copy from one reg to another
     'ST' : (1, [('reg', None)]),   # Store (to display/output)
-    'LI' : (2, [('reg', 'num')]),  # Load Immediate
+    'LI' : (2, [('num', 'reg')]),  # Load Immediate
     'DIS' : (3, [('num', None)]),  # Store to output
     }
 
@@ -25,7 +26,7 @@ class CompilerError(Exception):
 
 
 def pack_data(input_val):
-    if not input_val in range(0, 15):
+    if not input_val in range(0, 16):
         raise CompilerError("data argument is not a short")
 
     return input_val
@@ -85,26 +86,61 @@ def parse_operation(asm_line):
     if match:
         g = match.groupdict()
 
-        g['arg1'] = parse_argument(g['arg1'])
-        if g['arg2']:
-            g['arg2'] = parse_argument(g['arg2'])
-        else:
-            del g['arg2']
-
+        g['arg1'] = parse_argument(g['arg1']) if g['arg1'] else (None, None)
+        g['arg2'] = parse_argument(g['arg2']) if g['arg2'] else (None, None)
 
         return g
 
 
-def interperate_line(asm_line):
-    pass
+def make_bytecode(asm_line):
+    asm_dict = parse_operation(asm_line)
+    
+    if not asm_dict:
+        raise CompilerError('line is not valid')
+
+    arg_tuple = (asm_dict['arg1'][0], asm_dict['arg2'][0])
+    val_tuple = (asm_dict['arg1'][1], asm_dict['arg2'][1])
+
+    if not arg_tuple in opcodes[asm_dict['op']][1]:
+        raise CompilerError('incorrect arguments for %s' % asm_dict['op'])
+    
+    op = asm_dict['op']
+
+    src = 0
+    data = 0
+    if 'reg' == arg_tuple[0]:
+        src = val_tuple[0]
+        data = 0
+    elif 'num' == arg_tuple[0]:
+        src = 0
+        data = val_tuple[0]
+   
+    dest = 0 
+    if 'reg' == arg_tuple[1]:
+        dest = val_tuple[1]
+    elif 'num' == arg_tuple[1]:
+        dest = 0
+    
+    print op, src, dest, data
+
+    pdata = pack_data(int(data))
+    pcmd = pack_cmd(op, int(src), int(dest))
+
+    return struct.pack('BB', pcmd, pdata)
+
 
 if __name__ == "__main__":
-    test = """
-NOPE #0, #1
-ST #0,#1
-ST R1,R2
-DIS R0,#3
-LI #3
-"""
-    for l in test.splitlines():
-        print parse_operation(l)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('in_file', type=str)
+    parser.add_argument('out_file', type=str)
+
+    args = parser.parse_args()
+
+    with open(args.in_file, 'r') as in_file:
+        with open(args.out_file, 'w') as out_file:
+            for l in in_file:
+                try:
+                    out_file.write(make_bytecode(l))
+                except CompilerError as ce:
+                    print str(ce), l
+                    break
